1d70704f942bb6d8347eaa5e53b2f5a39857657b
arya2004 <arya20j4@gmail.com>
2024-12-02 18:50:46 +0530

    feat: add Player enum with extensions

---
+++ ChessLogic/Player.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace ChessLogic
+{
+    public enum Player
+    {
+        None,
+        Black,
+        White
+    }
+
+    public static class PlayerExtensions
+    {
+        public static Player Opponent(this Player player)
+        {
+            switch (player)
+            {
+                case Player.White:
+                    return Player.Black;
+                case Player.Black:
+                    return Player.White;
+                default:
+                    return Player.None;
+            }
+        } 
+    }
+}
