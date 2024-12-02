1f02949d726c3b111e2fa6bc207968bbbefa6985
arya2004 <arya20j4@gmail.com>
2024-12-03 00:55:23 +0530

    Add Images class and update MainWindow for chess UI

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using ChessLogic;
+using ChessLogic.Pieces;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,40 @@ namespace ChessUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Image[,] pieceImage = new Image[8, 8];
+        private GameState gameState;
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBoard();
+
+            gameState = new GameState(Player.White, Board.Initial());
+            DrawBoard(gameState.Board);
+        }
+
+        private void InitializeBoard()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Image image = new Image();
+                    pieceImage[i, j] = image;
+                    PieceGrid.Children.Add(image);
+                }
+            }
+        }
+
+        private void DrawBoard(Board board)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Piece piece = board[i, j];
+                    pieceImage[i, j].Source = Images.GetImage(piece);
+                }
+            }
         }
     }
 }
