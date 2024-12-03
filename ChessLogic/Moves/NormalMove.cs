3655af5beea90db87cbb8033b05faaafa17a2809
arya2004 <arya20j4@gmail.com>
2024-12-03 12:07:15 +0530

    feat: Add MoveType enum, Move classes, and update Piece class

---
+++ ChessLogic/Moves/NormalMove.cs
@@ -0,0 +1,27 @@
+﻿using ChessLogic.Pieces;
+
+namespace ChessLogic.Moves
+{
+    public class NormalMove : Move
+    {
+        public override MoveType Type => MoveType.Normal;
+
+        public override Position FromPosition { get; }
+
+        public override Position ToPosition { get; }
+
+        public NormalMove(Position from, Position to)
+        {
+            this.ToPosition = to;
+            this.FromPosition = from;
+        }
+
+        public override void Execute(Board board)
+        {
+            Piece piece = board[FromPosition];
+            board[ToPosition] = piece;
+            board[FromPosition] = null;
+            piece.HasMoved = true;
+        }
+    }
+}
--- ChessLogic/Pieces/Piece.cs
+++ ChessLogic/Pieces/Piece.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessLogic.Moves;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,29 @@ namespace ChessLogic.Pieces
         public abstract Player Color { get;  }
         public bool HasMoved { get; set; } = false;
         public abstract Piece Copy();
+        public abstract IEnumerable<Move> GetMoves(Position from, Board board);
+        protected IEnumerable<Position> MovePositionInDirection(Position from, Board board, Direction direction)
+        {
+            for (Position pos = from + direction; Board.IsInside(pos); pos += direction)
+            {
+                if (board.IsEmpty(pos))
+                {
+                    yield return pos;
+                    continue;
+                }
+                Piece piece = board[pos];   
+
+                if(piece.Color != Color)
+                {
+                    yield return pos;
+                }
+                yield break;
+            }
+        }
+
+        protected IEnumerable<Position> MovePositionInDirections(Position from, Board board, Direction[] directions)
+        {
+            return directions.SelectMany(dir => MovePositionInDirection(from, board, dir));
+        }
     }
 }
